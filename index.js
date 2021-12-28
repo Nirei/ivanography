55119be9bf6eb2870de08ce3b2bdf15d009d25de
Nirro <nireiorange@gmail.com>
2021-12-28 01:43:47 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,119 @@
+"use strict";
+
+const ROW_LENGTH = 20;
+const COL_LENGTH = 10;
+const GRID_SIZE = ROW_LENGTH * COL_LENGTH;
+
+const LEGAL_CHARACTERS = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "Ñ",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+const LOW_CONTRAST = [
+  { foreground: "#ffffff", background: "#ffffff" },
+  { foreground: "#ffffff", background: "#ffffff" },
+  { foreground: "#ffffff", background: "#ffffff" },
+  { foreground: "#ffffff", background: "#ffffff" },
+  { foreground: "#93c47d", background: "#a2c4c9" },
+  { foreground: "#b6d7a8", background: "#f6b26b" },
+  { foreground: "#93c47d", background: "#ffd966" },
+  { foreground: "#674ea7", background: "#3c78d8" },
+  { foreground: "#ead1dc", background: "#c9daf8" },
+  { foreground: "#ead1dc", background: "#d9d2e9" },
+  { foreground: "#a2c4c9", background: "#93c47d" },
+  { foreground: "#f6b26b", background: "#b6d7a8" },
+  { foreground: "#ffd966", background: "#93c47d" },
+  { foreground: "#3c78d8", background: "#674ea7" },
+  { foreground: "#c9daf8", background: "#ead1dc" },
+  { foreground: "#d9d2e9", background: "#ead1dc" },
+];
+
+const HIGH_CONTRAST = [
+  { foreground: "#ffd966", background: "#674ea7" },
+  { foreground: "#674ea7", background: "#ffd966" },
+  { foreground: "#6fa8dc", background: "#ffd966" },
+  { foreground: "#ffd966", background: "#6fa8dc" },
+  { foreground: "#3c78d8", background: "#ead1dc" },
+  { foreground: "#ead1dc", background: "#3c78d8" },
+  { foreground: "#b6d7a8", background: "#674ea7" },
+  { foreground: "#674ea7", background: "#b6d7a8" },
+];
+
+function choice(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function slot(character, colors) {
+  return {
+    character,
+    colors,
+  };
+}
+
+function generateRandomSlotBuffer() {
+  const array = [];
+  for (let i = 0; i < GRID_SIZE; i += 1) {
+    array.push(slot(choice(LEGAL_CHARACTERS), choice(LOW_CONTRAST)));
+  }
+
+  return array;
+}
+
+function setCell(y, x, slot) {
+  const td = document.getElementById("grid-body").children[y].children[x];
+  td.style.color = slot.colors.foreground;
+  td.style.backgroundColor = slot.colors.background;
+  td.textContent = slot.character;
+}
+
+function cleanUp(input) {
+  const noDiacritcs = input.normalize("NFD").replace(/\p{Diacritic}/gu, "");
+  const noPunctuation = noDiacritcs.replace(/[^a-zA-Zñ]/, "");
+  return noPunctuation.toUpperCase();
+}
+
+function handleInput(value) {
+  const message = cleanUp(value).split("");
+  const slotBuffer = generateRandomSlotBuffer();
+
+  for (let i = 0; i < message.length; i += 1) {
+    const chunk = GRID_SIZE / message.length;
+    const index = i * chunk + Math.floor(Math.random() * chunk);
+    slotBuffer[index] = slot(message[i], choice(HIGH_CONTRAST));
+  }
+
+  // Draw each slot on its corresponding cell
+  slotBuffer.forEach((slot, index) => {
+    const y = Math.floor(index / ROW_LENGTH);
+    const x = index % ROW_LENGTH;
+
+    setCell(y, x, slot);
+  });
+}
+
+handleInput("");
